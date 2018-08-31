130f2b225d09c35fe840e07023dcc1687cf58b6e
kelseyandersen <andersenk@ufl.edu>
2018-08-31 10:15:36 -0400

    code

--- HaitiMapping.R
+++ HaitiMapping.R
@@ -13,3 +13,14 @@ join <- read.csv("haiticsv.csv")
 haiti@data <- full_join(haiti@data, join, by = "NAME_1")
 
 tmap::qtm(shp = haiti, fill = "Incidence", fill.palette = "RdYlGn") 
+
+library(usethis)
+
+usethis::use_git()
+usethis::use_github()
+browse_github_pat()
+cat("GITHUB_PAT=18dffc7330766760fb5429ead5c71112b1219375",
+    file=file.path(normalizePath("~/"), ".Renviron"),
+    append=TRUE)
+Sys.getenv("GITHUB_PAT")
+devtools::install_github('kelseyandersen/haitinetwork')
